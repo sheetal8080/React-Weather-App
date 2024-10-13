d6f1f97dd554210f7fa4b8ab5cd4ede62ef52ad2
sheetal8080 <113094928+sheetal8080@users.noreply.github.com>
2024-10-13 18:15:14 +0530

    updated

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -4,10 +4,8 @@ export default function Credit() {
   return (
     <div class="credit">
     <p>
-      Coded by
-      <a href="https://www.shecodes.io/graduates/73033-chaimae-lamirine"
-        > Chaimae Lamirine</a
-      >, <a href="https://github.com/shemmee/React-Weather-App">Open sourced on Github.</a> Hosted on Netlify
+      Coded by Sheetal Tripathi
+      <a href="https://github.com/sheetal8080/React-Weather-App">Open sourced on Github.</a>
     </p>
   </div>
   );
