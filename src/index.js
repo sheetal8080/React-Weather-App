44a7a447fb7b614ec6d5fdf3fe6da37a250c3394
Sheetal Tripathi <>
2024-10-13 17:14:58 +0530

    updated layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import App from "./components/App";
+import Credit from "./components/Credit";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+    <Credit />
+  </StrictMode>,
+  rootElement
+);
